19feee5e5f6ee4c933d214c7a024d6215cd39fba
mohitkris <mohitkri@andrew.cmu.edu>
2014-12-09 22:43:07 -0500

    Updated the Home.html Search button

--- Moviq/Js/views/headerVw.js
+++ Moviq/Js/views/headerVw.js
@@ -1,7 +1,7 @@
 /**/
 
 /*global define*/
-<<<<<<< HEAD
+
 define('views/headerVw', {
     init: function ($, routes) {
         "use strict";
@@ -44,47 +44,4 @@ define('views/headerVw', {
         $('span.close').click(function () { $(hiddenSection).fadeOut(); });
     }
 });
-=======
-define('views/headerVw', { init: function ($, routes) {
-    "use strict";
-    
-    var selector = 'form.search-form',
-        inputSelector = selector + ' input';
-    
-    // add singleton events
-    $(document).on('submit', selector, function (event) {
-        routes.navigate('/search/?q=' + $(inputSelector).val());
-        return false;
-    });
 
-    $(document).on('submit', 'form.exampleform', function (event) {
-        Stripe.setPublishableKey('pk_test_RPfyHYbopF6CfB00Wj2IokwY');
-
-        Stripe.createToken({
-            number: $('.card-number').val(),
-            cvc: $('.card-cvc').val(),
-            exp_month: $('.card-expiry-month').val(),
-            exp_year: $('.card-expiry-year').val()
-        }, function (status, response) {
-            if (response.error) {
-                // re-enable the submit button
-                // $(form['submit-button']).removeAttr("disabled")
-                // show the error
-                alert(response.error.message);
-                $(".payment-errors").html(response.error.message);
-
-
-
-            } else {
-                // token contains id, last4, and card type
-                var token = response['id'];
-
-                routes.navigate('/stripe/?q=' + token + '&email=' + $('.email').val());
-                return false;
-
-            }
-        });
-
-    });
-}});
->>>>>>> develop_mohit
