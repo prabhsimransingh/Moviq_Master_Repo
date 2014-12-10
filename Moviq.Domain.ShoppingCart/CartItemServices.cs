e04b73dfa716d40006f2a931c7e6452a3427f273
vaibhavi04 <phadnisvaibhavi@gmail.com>
2014-12-09 22:46:44 -0800

    Cart Functinality for logged in user

---
+++ Moviq.Domain.ShoppingCart/CartItemServices.cs
@@ -0,0 +1,41 @@
+﻿using Moviq.Interfaces.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moviq.Domain.Products
+{
+   public class CartItemServices : ICartItemServices
+    {
+        public CartItemServices(ICartRepository cartRepo) 
+        {
+            this.cartRepo = cartRepo;
+        }
+
+        ICartRepository cartRepo;
+
+       public ICartItem AddCartItem(ICartItem item)
+        {
+            return cartRepo.Set(item);
+        }
+        public ICartItem UpdateCartItem(ICartItem item)
+        {
+           var result = cartRepo.GetByGuid(item.Guid);
+           result.AddedFlag = true;
+           return cartRepo.Set(item);
+       }
+
+       public bool EmptyCart(string userId)
+        {
+           var result = cartRepo.GetByUserId(userId);
+           foreach(CartItem record in result)
+           {
+            record.AddedFlag = true;
+            cartRepo.Set(record);
+           }
+            return true;
+        }
+    }
+}
